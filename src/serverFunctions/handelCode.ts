276fc75b86049e734a56d69aae8302511af97126
S B Rakesh Rath <work.sbrakeshrath@gmail.com>
2025-04-23 11:05:24 +0530

    refactor handelCode function to comment out unused code and streamline response handling

--- src/serverFunctions/handelCode.ts
+++ src/serverFunctions/handelCode.ts
@@ -8,78 +8,80 @@ import axios from "axios";
 
 export default async function handelCode(value: string) {
   try {
-    initAdmin();
+    // initAdmin();
 
-    const filePath = SaveCodeToFile(value, "del");
-    console.log("File saved at:", filePath);
+    // const filePath = SaveCodeToFile(value, "del");
+    // console.log("File saved at:", filePath);
 
-    const bucket = getStorage().bucket();
+    // const bucket = getStorage().bucket();
 
-    // Upload the file to Firebase Storage
-    const res = await bucket.upload(filePath, {
-      metadata: {
-        contentType: "text/plain",
-      },
-    });
+    // // Upload the file to Firebase Storage
+    // const res = await bucket.upload(filePath, {
+    //   metadata: {
+    //     contentType: "text/plain",
+    //   },
+    // });
 
-    const downloadUrl = await res[0].getSignedUrl({
-      action: "read",
-      expires: Date.now() + 24 * 60 * 60 * 1000, // 1 day
-      version: "v4",
-    });
+    // const downloadUrl = await res[0].getSignedUrl({
+    //   action: "read",
+    //   expires: Date.now() + 24 * 60 * 60 * 1000, // 1 day
+    //   version: "v4",
+    // });
     // console.log("File uploaded to Firebase Storage:", downloadUrl[0]);
     // create a db entry with the download URL
-    const db = getFirestore("code-compilation");
-    const docRef = db.collection(
-      process.env.NEXT_PUBLIC_COLLECTION_NAME as string
-    );
+    // const db = getFirestore("code-compilation");
+    // const docRef = db.collection(
+    //   process.env.NEXT_PUBLIC_COLLECTION_NAME as string
+    // );
 
-    const dataRes = await db.collection("code-compile").add({
-      InputfileLink: downloadUrl[0],
-      status: "queued",
-      finishedAt: null,
-      language: "delirium",
-      outputFileLink: null,
-      codeStatus: "pending",
-      createdAt: Timestamp.now(),
-    });
-    const docId = dataRes.id;
-    console.log("Document written with ID: ", docId);
+    // const dataRes = await db.collection("code-compile").add({
+    //   InputfileLink: downloadUrl[0],
+    //   status: "queued",
+    //   finishedAt: null,
+    //   language: "delirium",
+    //   outputFileLink: null,
+    //   codeStatus: "pending",
+    //   createdAt: Timestamp.now(),
+    // });
+    // const docId = dataRes.id;
+    // console.log("Document written with ID: ", docId);
 
-    const url = "https://compiler-test-1084173791820.us-central1.run.app";
+    const url = "https://compiler-test-1084173791820.us-central1.run.app/direct-code-runner";
     const response = await axios.post(url, {
-      docID: docId,
+      code: value,
     });
     if (response.status !== 200) {
       return false;
     }
+    // console.log("Response data:", response.data);
     // refetch the document to get the output file link
 
-    const docRef2 = db.collection("code-compile").doc(docId);
-    const doc = await docRef2.get();
-    if (!doc.exists) {
-      console.log("No such document!");
-      return false;
-    }
-    const data = doc.data();
-    if (!data) {
-      console.log("No data found!");
-      return false;
-    }
-    const outputFileLink = data.outputFileLink;
+    // const docRef2 = db.collection("code-compile").doc(docId);
+    // const doc = await docRef2.get();
+    // if (!doc.exists) {
+    //   console.log("No such document!");
+    //   return false;
+    // }
+    // const data = doc.data();
+    // if (!data) {
+    //   console.log("No data found!");
+    //   return false;
+    // }
+    // const outputFileLink = data.outputFileLink;
 
-    // download the output file from link and get the tetxt content
-    const outputFile = await axios.get(outputFileLink, {
-      responseType: "arraybuffer",
-    });
-    // const outputFileText = Buffer.from(outputFile.data, "binary").toString(
-    //   "utf-8"
-    // );
-    const outputFileText = Buffer.from(outputFile.data).toString("utf-8");
+    // // download the output file from link and get the tetxt content
+    // const outputFile = await axios.get(outputFileLink, {
+    //   responseType: "arraybuffer",
+    // });
+    // // const outputFileText = Buffer.from(outputFile.data, "binary").toString(
+    // //   "utf-8"
+    // // );
+    // const outputFileText = Buffer.from(outputFile.data).toString("utf-8");
 
-    console.log("Output file text:", outputFileText);
+    // console.log("Output file text:", outputFileText);
 
-    return outputFileText;
+    // return outputFileText;
+    return response.data.codeResponse.codeResponse;
   } catch (error) {
     console.error(
       "Error during file upload or Firebase storage interaction:",
